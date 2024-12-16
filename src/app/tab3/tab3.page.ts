80df2141483039239a6a3e7026aecaf3874cd013
rlaurido <153690410+rlaurido@users.noreply.github.com>
2024-12-16 14:21:43 -0500

    first

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { /* Importe los componentes */
+  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+  IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: true,
+  imports: [  /* Registre los componentes */
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+    IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+})
+export class Tab3Page {
+  constructor() {}
+}
